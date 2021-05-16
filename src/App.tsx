0428e06dd1932e11f16fe4233a176e32a2216eec
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-16 19:01:55 +0930

    chore: Configure Typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Get fricked</h1>
+    </header>
+  </div>
+);
+
+export default App;
