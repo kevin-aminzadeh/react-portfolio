adbe7492b876d4a21c166479c022864349769cc9
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-18 00:03:12 +0930

    fix: Address project card image size variation issue

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -12,9 +12,9 @@ const ProjectCard: FunctionComponent<ProjectCardProps> = ({
   imageSrc,
 }: ProjectCardProps) => (
   <div className="col-xs-12 col-md-6 col-lg-4 p-3">
-    <div className="card bg-dark text-white border-0">
-      <a href={url} className="stretched-link">
-        <img className="card-img" src={imageSrc} alt={`${name} Demo`} />
+    <div className="card bg-dark text-white border-0 h-100">
+      <a href={url} className="stretched-link h-100">
+        <img className="card-img h-100" src={imageSrc} alt={`${name} Demo`} />
       </a>
     </div>
   </div>
