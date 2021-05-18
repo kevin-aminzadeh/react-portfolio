4b95374b078cbd515d5580b9d621216954ac24ce
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-18 13:01:19 +0930

    fix: Address ProjectCard image reflow issue

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -11,10 +11,25 @@ const ProjectCard: FunctionComponent<ProjectCardProps> = ({
   url,
   imageSrc,
 }: ProjectCardProps) => (
-  <div className="col-xs-12 col-md-6 col-lg-4 p-3">
-    <div className="card bg-dark text-white border-0 h-100">
-      <a href={url} className="stretched-link h-100">
-        <img className="card-img h-100" src={imageSrc} alt={`${name} Demo`} />
+  <div
+    className="col-xs-12 col-md-6 col-lg-4 p-3"
+    style={{ minHeight: '214px' }}
+  >
+    <div
+      className="card bg-dark text-white border-0 h-100"
+      style={{ minHeight: '100%' }}
+    >
+      <a
+        href={url}
+        className="stretched-link h-100"
+        style={{ paddingBottom: '56.25%' }}
+      >
+        <img
+          className="card-img h-100"
+          src={imageSrc}
+          alt={`${name} Demo`}
+          style={{ minHeight: '100%', position: 'absolute' }}
+        />
       </a>
     </div>
   </div>
